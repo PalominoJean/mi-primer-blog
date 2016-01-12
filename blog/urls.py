6bf3fb52a2d90e49e096fa5720927eb47a2e4951
Jean <zikvanz@gmail.com>
2016-01-12 17:02:47 -0500

    se anadio templates de cada post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from blog import views
 
 urlpatterns=[
 	url(r'^$',views.post_lista),
+	url(r'^post/(?P<post_pk>[0-9]+)/$',views.post_detalle),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 
@@ -6,3 +6,7 @@ from blog.models import Post
 def post_lista(request):
 	posts=Post.objects.filter(publicacion_fecha__lte=timezone.now()).order_by('publicacion_fecha')
 	return render(request,'blog/post_lista.html',{'posts':posts})
+
+def post_detalle(request,post_pk):
+	post=get_object_or_404(Post,pk=post_pk)
+	return render(request, 'blog/post_detalle.html',{'post':post})
