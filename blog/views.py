7436f4f5995c82af6b1ebee796a546b0d459afa8
Jean <zikvanz@gmail.com>
2016-01-11 19:15:08 -0500

    mififique las plantillasy las views haciendo que se conecte con la bd.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
 
 # Create your views here.
 def post_lista(request):
-	return render(request,'blog/post_lista.html',{})
+	posts=Post.objects.filter(publicacion_fecha__lte=timezone.now()).order_by('publicacion_fecha')
+	return render(request,'blog/post_lista.html',{'posts':posts})
