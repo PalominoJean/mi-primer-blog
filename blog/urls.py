feb5a0debe20b741ef18ade3a49f4f71703e6e8f
Jean <zikvanz@gmail.com>
2016-01-12 20:03:20 -0500

    anadi la funcinalidad editar post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from blog import views
 
 urlpatterns=[
-	url(r'^$',views.post_lista),
-	url(r'^post/(?P<post_pk>[0-9]+)/$',views.post_detalle),
+	url(r'^$', views.post_lista),
+	url(r'^post/(?P<post_pk>[0-9]+)/$', views.post_detalle),
+	url(r'^post/nuevo/$', views.post_nuevo, name='post_nuevo'),
+	url(r'^post/(?P<post_pk>[0-9]+)/editar/$', views.post_editar, name='post_editar'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Post
+from blog.forms import PostForm
+
 
 # Create your views here.
 def post_lista(request):
@@ -10,3 +12,29 @@ def post_lista(request):
 def post_detalle(request,post_pk):
 	post=get_object_or_404(Post,pk=post_pk)
 	return render(request, 'blog/post_detalle.html',{'post':post})
+
+def post_nuevo(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.autor = request.user
+			post.publicacion_fecha = timezone.now()
+			post.save()
+			return redirect('blog.views.post_detalle', post_pk=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_nuevo.html', {'form': form})	
+
+def post_editar(request,post_pk):
+	post=get_object_or_404(Post,pk=post_pk)
+	if request.method=="POST":
+		form=PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post=form.save(commit=False)
+			post.autor=request.user
+			post.save()
+			return redirect('blog.views.post_detalle',post_pk=post.pk)
+	else:
+		form=PostForm(instance=post)
+	return render(request,'blog/post_nuevo.html',{'form':form})
